30d4322482717334c312d2414541ff65c59d12bd
ffiargus <rm.chow@outlook.com>
2017-05-08 15:39:57 +0000

    completes step 4

---
+++ fourthFunction.js
@@ -0,0 +1,36 @@
+var https = require('https');
+
+
+function getHTML (options, callback) {
+
+  /* Add your code here */
+  https.get(options, function (response) {
+    response.setEncoding('utf8');
+
+    var bufferString = '';
+
+    response.on('data', function(data) {
+      console.log('Chunk received. Length:',data.length);
+      bufferString += data;
+    });
+
+
+    response.on('end', function() {
+      callback(bufferString);
+      console.log('Response stream complete.');
+    });
+
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
