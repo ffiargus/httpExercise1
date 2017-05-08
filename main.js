5f61dad838c7adafee52dca6ccbf686fac163274
ffiargus <rm.chow@outlook.com>
2017-05-08 15:12:14 +0000

    Uses a bufferString to print text

--- main.js
+++ main.js
@@ -2,19 +2,22 @@ var https = require('https');
 
 var requestOptions = {
   host: 'sytantris.github.io',
-  path: '/http-examples/step1.html'
+  path: '/http-examples/step2.html'
 };
 
 https.get(requestOptions, function (response) {
   response.setEncoding('utf8');
 
+  var bufferString = '';
+
   response.on('data', function(data) {
     console.log('Chunk received. Length:',data.length);
-    console.log(data);
+    bufferString += data;
   });
 
 
   response.on('end', function() {
+    console.log(bufferString);
     console.log('Response stream complete.');
   });
 
